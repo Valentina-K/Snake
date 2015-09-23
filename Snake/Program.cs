f1e3868330c2b361c817e70c78082dc01f8bdb8d
Valentina-K <kvalentyna@hotmail.com>
2015-09-23 15:47:20 +0300

    Added users dialog

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,9 +12,15 @@ namespace Snake
         
         static void Main(string[] args)
         {
+            int t;
+            Console.WriteLine("Выберите свой уровень:\nвысокий - 1, средний - 2 или низкий - 3");
+            ConsoleKeyInfo cinf= Console.ReadKey();
+            t = (cinf.KeyChar - 48) * 100;
+            Console.Clear();
             Console.SetBufferSize(80, 25);
             Walls wall = new Walls(80, 25);
             wall.Drow();
+                    
             Point p1 = new Point(3, 3, '*');
             Snake sn = new Snake(p1, 5, Direction.RIGHT);
             sn.Draw();
@@ -32,7 +38,7 @@ namespace Snake
                 }
                 else
                     sn.Move(); 
-                Thread.Sleep(200);
+                Thread.Sleep(t);
 
                 if (Console.KeyAvailable) 
                 {
@@ -40,9 +46,9 @@ namespace Snake
                     sn.HandleKey(key.Key);
                 }
             }
-            
-            
-            //Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Игра окончена! \nСпасибо за внимание.");
+            Console.ReadLine();
         }
     }
 }
