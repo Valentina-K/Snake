7ee991045d6859946fe39537db47948a5bb8dd7b
Valentina-K <kvalentyna@hotmail.com>
2015-09-23 09:29:00 +0300

    Created class Figure

--- Snake/VerticalLine.cs
+++ Snake/VerticalLine.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine : Figure
+    class VerticalLine : Figure
     {
-        public HorizontalLine(int xLeft, int xRight, int y, char sim) 
+        public VerticalLine(int yUp, int yUdown, int x, char sim) 
         {
             pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++) 
+            for (int y = yUp; y <= yUdown; y++) 
             {
                 Point p = new Point(x, y, sim);
                 pList.Add(p);
